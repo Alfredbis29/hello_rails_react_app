9d49e1d0399044c9d3c1bc7cccc45c48a6e86877
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2022-08-19 12:59:34 +0200

    connect webpack

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
     @greetings = Greeting.all
-    render json: @greetings
+    if @greetings.empty?
+      json_response({ msg: 'no greeting found' }, 400)
+    else
+      render json: @greetings[rand(0...@greetings.length - 1)]
     end
+  end
 end
